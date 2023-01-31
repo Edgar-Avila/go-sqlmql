b422e0c24e103d60d299a8636ef14d94c2cbfefe
Edgar <avila.agramonte.edg@gmail.com>
2023-01-31 13:47:05 -0500

    fix: Translate multiple statements

--- translate/sql_file.go
+++ translate/sql_file.go
@@ -2,26 +2,37 @@ package translate
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/Edgar-Avila/go-sqlmql/parser"
 )
 
+
+func TranslateStatement(statement parser.Statement) (string, error) {
+    switch s := statement.(type) {
+    case *parser.CreateStatement:
+        return TranslateCreate(s)
+    case *parser.DropStatement:
+        return TranslateDrop(s)
+    case *parser.InsertStatement:
+        return TranslateInsert(s)
+    case *parser.SelectStatement:
+        return TranslateSelect(s)
+    case *parser.UpdateStatement:
+        return TranslateUpdate(s)
+    default:
+        return "", fmt.Errorf("Invalid statement type %T", s)
+    }
+}
+
 func TranslateSqlFile(sqlFile *parser.SqlFile) (string, error) {
+    translated := make([]string, 0)
 	for _, statement := range sqlFile.Statements {
-		switch s := statement.(type) {
-		case *parser.CreateStatement:
-			return TranslateCreate(s)
-		case *parser.DropStatement:
-			return TranslateDrop(s)
-        case *parser.InsertStatement:
-            return TranslateInsert(s)
-        case *parser.SelectStatement:
-            return TranslateSelect(s)
-        case *parser.UpdateStatement:
-            return TranslateUpdate(s)
-		default:
-			return "", fmt.Errorf("Invalid statement type %T", s)
-		}
+        t, err := TranslateStatement(statement)
+        if err != nil {
+            return "", err
+        }
+        translated = append(translated, t)
 	}
-	return "", nil
+	return strings.Join(translated, "\n"), nil
 }
